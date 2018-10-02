21b1476a8b98d633647877f542aaf7b94f5fa01b
Obed Lorisson <Obed.lorisson@gmail.com>
2018-10-01 22:25:14 -0400

    some minor ui enhancement

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,6 +2,7 @@ import React from "react";
 
 class TodoForm extends React.Component {
   render() {
+    console.log(this.props)
     return (
       <div className="form">
         <div className="form-input">
